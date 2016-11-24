edb971b397942141454823e4f41658af29b95f44
Joel Williams <joel.b.williams.is@gmail.com>
2016-11-23 22:31:41 -0600

    Iterate over names/aliases when finished

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -32,6 +32,7 @@ end
 
 puts "That you agent?"
 puts ""
+little_black_book = {}
 until gets.chomp == "quit"
 	puts "What is your last name, agent?"
 	agent_last = gets.chomp
@@ -40,6 +41,9 @@ until gets.chomp == "quit"
 	agent_alias = create_alias(agent_first, agent_last)
 	puts "You'll get a belly full'a lead in no time flat goin' by a name like that..."
 	puts "Tell ya what, you can be #{agent_alias}.  Yeah, that'll do."
+	little_black_book[agent_alias]=[agent_first, agent_last]
 	puts ""
 	puts "Got a friend with you there?  Step on up. ('quit' to exit)"
 end
+
+little_black_book.each {|fake, real| puts "Agent #{real[0]} #{real[1]} is going by the name of #{fake}."}
