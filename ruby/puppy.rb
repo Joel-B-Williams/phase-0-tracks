0bab45822a44ddac02d3104c1e59f82fb008a269
Joel B. Williams <joel.b.williams.is@gmail.com>
2016-11-28 11:34:37 -0600

    Create puppy file

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#Puppy Characteristics:
+#Bark magnitude
+#Ear floopyness
+#Romp level
+#Color
+#Name
+
+#Puppy Behaviors:
+#Pee on the floor
+#Tear up everything.  Literally everything.
+#Look adorable until you love it again
+
+class Puppy
+end
+
