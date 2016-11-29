d5f44b3933b04ee941249b2aeffcf022629c4d90
Joel B. Williams <joel.b.williams.is@gmail.com>
2016-11-29 16:16:00 -0600

    Create santa class and base methods

---
+++ ruby/santa.rb
@@ -0,0 +1,15 @@
+class Santa
+	
+	def speak
+		puts "Ho, ho ho! Haaaaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{coookie}!"
+	end
+
+	def initialize
+		puts "Initializing Santa instanct..."
+	end
+	
+end
