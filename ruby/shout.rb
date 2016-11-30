0373ec144aa8b5146b7852291c27b9fa40753c4c
Joel B. Williams <joel.b.williams.is@gmail.com>
2016-11-30 12:08:34 -0600

    include Shout method on two classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,36 @@
 module Shout
-	def self.yell_angrily(words)
-		words+"!!!"+":C"
+	#Standalone Shout module methods
+	# def self.yell_angrily(words)
+	# 	words+"!!!"+":C"
+	# end
+
+	# def self.yell_happily(words)
+	# 	words+"!!!"+"C:"
+	# end
+	def yell_angrily(words)
+		puts words+"!!!"+":C"
 	end
 
-	def self.yell_happily(words)
-		words+"!!!"+"C:"
+	def yell_happily(words)
+		puts words+"!!!"+"C:"
 	end
 end
 
-puts Shout.yell_happily("I can't believe you did this")
-puts Shout.yell_angrily("I can't believe you did this")
-puts "You see?  The faces make all the difference!"
+# puts Shout.yell_happily("I can't believe you did this")
+# puts Shout.yell_angrily("I can't believe you did this")
+# puts "You see?  The faces make all the difference!"
+class Parent
+	include Shout	
+end
+
+class Teacher
+	include Shout
+end
+
+parent = Parent.new
+parent.yell_angrily("You did this")
+parent.yell_happily("You did this")
+
+teacher = Teacher.new
+teacher.yell_happily("You did this")
+teacher.yell_angrily("You did this")
