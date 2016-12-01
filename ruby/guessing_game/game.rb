d67f0e4bb28cfc5563bb50916afe3dba2b4298c8
Joel Williams <joel.b.williams.is@gmail.com>
2016-12-01 13:36:40 -0600

    pseudocode game class

---
+++ ruby/guessing_game/game.rb
@@ -0,0 +1,6 @@
+# Game class guessing game
+# Initialize a new game with the secret word as a variable
+# limit the number of guesses -> relate to length of word
+# repeated guesses do not count against player (store guesses and compare)
+# print feedback after each guess (wheel of fortune style/hangman style)
+# display appropriate message at end of game
