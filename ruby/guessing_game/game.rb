4207394d9a9a4404dd752b9e802867231f0abed5
Joel Williams <joel.b.williams.is@gmail.com>
2016-12-01 14:53:42 -0600

    Verify method to add secret word

--- ruby/guessing_game/game.rb
+++ ruby/guessing_game/game.rb
@@ -4,3 +4,9 @@
 # repeated guesses do not count against player (store guesses and compare)
 # print feedback after each guess (wheel of fortune style/hangman style)
 # display appropriate message at end of game
+
+class Game
+	def secret_word(word)
+		@secret_word = word
+	end
+end
--- ruby/guessing_game/game_rspec.rb
+++ ruby/guessing_game/game_rspec.rb
@@ -2,3 +2,12 @@
 #'describe' block for each group of tests
 	#'it' block for each individual test
 		#expect(CODE HERE).to eq <RESULT>
+
+require_relative 'game'
+
+describe Game do
+	let(:game) {Game.new}
+	it "establishes secret word" do
+		expect(game.secret_word("word")).to eq "word"
+	end
+end
