1e65c271ecc8b407223c161886808b4ca3df6c44
Joel Williams <joel.b.williams.is@gmail.com>
2016-12-01 21:29:32 -0600

    Verified all tests

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,14 +1,24 @@
 class TodoList
-
+	
 	def initialize(chores)
 		@chores = chores
 	end
 
 	def get_items
-		p @chores
+		@chores
 	end
 
 	def add_item(chore)
 		@chores << chore
+	end	
+
+	def delete_item(chore)
+		@chores.delete(chore)
+	end
+
+	def get_item(index_value)
+		@chores[index_value]
 	end
 end
+
+
