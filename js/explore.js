c79c12cbf83514cbcbcd6bc48c26d30a83e31ff6
Joel Williams <joel.b.williams.is@gmail.com>
2016-12-06 10:42:54 -0600

    Pseudocode challenge

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// we are building a .reverse method!  but not in ruby.  :C
+// for each letter in given word (henceforth "word")
+// add last index into new variable (henceforth "newWord")
+// return newWord (because JS doesn't do it on its own)
+
