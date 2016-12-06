ab53bf4a15d0605e64643a367845a1c63337a6fb
Joel Williams <joel.b.williams.is@gmail.com>
2016-12-05 21:06:30 -0600

    Complete gps assignment

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,26 +4,31 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+#require_relative means that the file your looking for is in the same directory.
+#require allows you to access modules.
 require_relative 'state_data'
 
 class VirusPredictor
 
+  #When creating an instance we need to take an input of these 3 variables
+  #And we store that those variables for later use.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  #Print the results of predicted_results and speed_of_spread
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
+  #Anything below this is not accessible by the user.
   private
 
-  def predicted_deaths(population_density, population, state)
+  #Calculating the number of deaths, based on a calculations of the 3 varaibles.
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +46,8 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  #based on population density it sets the speed of spread
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -64,24 +70,39 @@ class VirusPredictor
 
 end
 
+
 #=======================================================================
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
+
+
+STATE_DATA.each do |state, pop_info|
+  state = VirusPredictor.new(state, pop_info[:population_density], pop_info[:population])
+  state.virus_effects
+end
+
+
 
 
 #=======================================================================
 # Reflection Section
+
+# - Functionally similar, using symbols does conserve memory, although using strings makes for more print-friendly data if necessary
+# - require_relative pulls from the same local directory, while require is used to pull gems/modules
+# - standard loop methods, as well as built in hash interation, can look at both key/value, only one, etc as needed
+# - variables were repeats of instance variables
+# - overall understanding of instance variables was helped, and seeing 'private' in action was good as well
