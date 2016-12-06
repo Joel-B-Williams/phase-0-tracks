f29b930e981ec314712675eb101c51ba8d356f80
Joel Williams <joel.b.williams.is@gmail.com>
2016-12-06 12:50:14 -0600

    Complete horse object for release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,13 @@
 var colors = ["red", "green", "blue", "yellow"];
 var names = ["Fred", "Dibbs", "One-piece", "Square Deal"];
 
+colors.push("brown");
+names.push("hoof-hearted");
+
+var horses = {};
+
+for (var i=0; i < names.length; i++) {
+	horses[names[i]] = colors[i];
+}
+
+console.log(horses)
