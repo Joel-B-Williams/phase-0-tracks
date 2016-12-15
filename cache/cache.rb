ba0d9fb96d3bcf0c8a6402475d87f318e749cb5d
Joel Williams <joel.b.williams.is@gmail.com>
2016-12-15 13:20:57 -0600

    Method to update expected income

--- cache/cache.rb
+++ cache/cache.rb
@@ -8,9 +8,24 @@ create_table = '
 CREATE TABLE IF NOT EXISTS budgets(
 id INTEGER PRIMARY KEY,
 cache INTEGER,
-expected_income INTEGER
+expected_income INTEGER,
 actual_income INTEGER
 )'
 
 db.execute(create_table) 
-#methods to retreive and manipulate table data
+
+add_user = '
+INSERT INTO budgets (cache, expected_income, actual_income)
+VALUES (0,0,0)'
+
+# db.execute(add_user) - want this to run only once for single user
+
+# --methods to retreive and manipulate table data--
+# method to set expected income 
+def set_expected_income(db, dolla_dolla_bills_yall)
+	expected_income = '
+	UPDATE budgets SET expected_income = ?
+	WHERE id = 1' 
+	db.execute(expected_income, [dolla_dolla_bills_yall]) 
+end
+
