3b771978246ca163cf394cc0b29fdcc36d85f3d7
Joel Williams <joel.b.williams.is@gmail.com>
2016-12-21 09:11:11 -0600

    Refactor selection code for buttons

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -4,20 +4,20 @@
 //2 to link buttons to appropriate shape - change shape
 // 1 to change color of clicked shape on click
 
-$(document).ready(function(){
+$(function(){
 	$('.shape').click(function(){
 		$(this).css("background-color", "blue")
 	})
 })
 
-$(document).ready(function(){
-	$('#one').click(function(){
+$(function(){
+	$('div button:first-child').click(function(){
 		$('#first').animate({height: "50px"})
 	})
 })
 
-$(document).ready(function(){
-	$('#two').click(function(){
+$(function(){
+	$('div button:last-child').click(function(){
 		$('#second').animate({width: "50px"})
 	})
 })
